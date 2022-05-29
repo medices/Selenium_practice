50d991a816f6d40ee852113a0db822d7f8c837c4
Natalie Mankovski <eurodrug@gmail.com>
2022-05-29 00:02:15 -0400

    Initial commit of the project.

---
+++ Selenium/src/com/edureka/basics/LaunchBrowser.java
@@ -0,0 +1,42 @@
+package com.edureka.basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	
+	public static WebDriver driver = null;
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Natalya\\eclipse-workspace\\Selenium\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.facebook.com");
+		
+		/*
+		 * driver.navigate().to("https://www.facebook.com"); 
+		 * driver.navigate().back();
+		 * driver.navigate().forward(); 
+		 * driver.navigate().refresh();
+		 */
+		
+		
+		String browserId = driver.getWindowHandle();
+		System.out.println(browserId);
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		
+		//driver.close(); // close current page
+		//driver.quit(); // close browser
+		
+		
+	}
+
+}
---
+++ Selenium/src/com/edureka/basics/LocatorsDemo.java
@@ -0,0 +1,73 @@
+package com.edureka.basics;
+
+
+
+
+import java.time.Duration;
+
+//import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class LocatorsDemo {
+	
+	public static WebDriver driver = null; 
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver",".//Drivers//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		
+		/*WebElement createNewAccountBtn = driver.findElement(By.name("Create new account"));
+		createNewAccountBtn.click();*/ //doesn't work
+		
+		/*WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys("nataly-m@tut.by");
+		
+		WebElement pwd = driver.findElement(By.name("pass"));
+		pwd.sendKeys("your_password");
+		
+		
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		
+		System.out.println(allLinks.size());
+		for(WebElement link: allLinks) {
+			String text = link.getText();
+			System.out.println(text);
+		}*/
+		
+		//driver.findElement(By.linkText("Forgot password?")).click(); // the same as next
+		//driver.findElement(By.partialLinkText("password")).click(); // the same as previous
+		
+		driver.findElement(By.partialLinkText("account")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(2000));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span/input[@value='1']")));
+		WebElement femaleRadioBtn = driver.findElement(By.xpath("//span/input[@value='1']"));
+		femaleRadioBtn.click();
+		
+			
+	}
+
+	
+		
+	}
+		
+		//doesn't work
+		
+		/*WebElement email = driver.findElement(By.xpath("//input[@type='email']"));
+		email.sendKeys("myemail@gmail.com");*/
+		
+	//}
+
+	
+	
+
+
